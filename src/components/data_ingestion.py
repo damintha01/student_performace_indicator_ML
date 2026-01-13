3c72d42776c084e762106db74e4beff19cf69ace
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2026-01-14 00:27:47 +0530

    Implement data ingestion and model training components; add model evaluation utility

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
+
 @dataclass
 class DataIngestionConfig: #where we will save the data
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -37,5 +45,16 @@ class DataIngestion:
                    )
         except Exception as e:
             raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation
+
+    ModelTrainer=ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr,test_arr))
+
 
  
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+from sklearn.base import r2_score
 from src.exception import CustomException
 import dill #craete a pickel file
 
@@ -15,3 +16,24 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+    def evaluate_models(X_train, y_train, X_test, y_test, models):
+        try:
+            report = {}
+
+            for i in range(len(models)):
+                model = list(models.values())[i]
+
+                model.fit(X_train, y_train)  # Train model
+
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+                report[list(models.keys())[i]] = test_model_score
+
+            return report
+
+        except Exception as e:
+            raise CustomException(e, sys)
